5d395e9628c5e92f0acbc24e760a6cbc1b8e53a6
Rehan Ansari <rehan.a@indictranstech.com>
2024-04-19 18:09:13 +0530

    add accounting dimensions fields

--- accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
+++ accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
@@ -3,3 +3,25 @@ import frappe
 def before_save(doc,method=None):
     if doc.is_return and not doc.return_against:
         frappe.throw('Please create a purchase invoice before generating a debit note')
+        
+    pan = frappe.db.get_value('Supplier', doc.supplier, 'pan')
+    if not pan:
+        return
+
+    url = f"{frappe.utils.get_url()}/api/method/crm_ishan.api.lead_api.validate_docs"
+    params = {
+        "reference_id": doc.name,
+        "document_type": 'PAN-DETAILED',
+        "id_number": pan
+    }
+    response = requests.get(url, params=params)
+    json_data = response.json()
+    frappe.errprint(json_data)
+    if json_data['message']['status'] == 'SUCCESS' and json_data['message']['kycStatus'] == 'SUCCESS':
+        if json_data['message']['kycResult']['idStatus'] == 'VALID' and json_data['message']['kycResult']['panStatus'] == 'VALID':
+            return
+    tds_section = frappe.db.get_value('Supplier', doc.supplier, 'tax_withholding_category.tds_section')
+    if tds_section:
+        non_pan_section = frappe.db.get_value('Tax Withholding Category', {'entity_type': 'No PAN / Invalid PAN', 'tds_section': tds_section})
+        if non_pan_section:
+            doc.tax_withholding_category = non_pan_section
--- accounts_ishan/accounts_ishan/custom_script/purchase_order.py
+++ accounts_ishan/accounts_ishan/custom_script/purchase_order.py
@@ -2,7 +2,6 @@ import frappe
 import requests
 
 def before_save(doc,method=None):
-    
     pan = frappe.db.get_value('Supplier', doc.supplier, 'pan')
     if not pan:
         return
@@ -13,14 +12,14 @@ def before_save(doc,method=None):
         "document_type": 'PAN-DETAILED',
         "id_number": pan
     }
-
-    # response = requests.get(url, params=params)
-    # json_data = response.json()
-    # frappe.errprint(json_data)
-    # id_status = json_data['message']['kycResult']['idStatus']
-
-    # if id_status == 'VALID':
-    #     return
+    response = requests.get(url, params=params)
+    json_data = response.json()
+    frappe.errprint(json_data)
+    if json_data['message']['status'] == 'SUCCESS' and json_data['message']['kycStatus'] == 'SUCCESS':
+        if json_data['message']['kycResult']['idStatus'] == 'VALID' and json_data['message']['kycResult']['panStatus'] == 'VALID':
+            return
     tds_section = frappe.db.get_value('Supplier', doc.supplier, 'tax_withholding_category.tds_section')
-    non_pan_section = frappe.db.get_value('Tax Withholding Category', {'entity_type': 'No PAN / Invalid PAN', 'tds_section': tds_section})
-    doc.tax_withholding_category = non_pan_section
+    if tds_section:
+        non_pan_section = frappe.db.get_value('Tax Withholding Category', {'entity_type': 'No PAN / Invalid PAN', 'tds_section': tds_section})
+        if non_pan_section:
+            doc.tax_withholding_category = non_pan_section

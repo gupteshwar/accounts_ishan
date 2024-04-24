a4c6972ceb1e7dd58f260b0dfd872d52f61d6ef1
rehan.a@indictranstech.com <rehan.a@indictranstech.com>
2024-04-24 11:38:38 +0530

    fix: api logic

--- accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
+++ accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
@@ -17,18 +17,21 @@ def before_save(doc,method=None):
     }
     response = requests.get(url, params=params)
     json_data = response.json()
-    if json_data['message']['status'] == 'SUCCESS':
-        if json_data['message']['kycStatus'] == 'SUCCESS':
-            if json_data['message']['kycResult']['idStatus'] == 'VALID':
-                if json_data['message']['kycResult']['panStatus'] == 'VALID':
-                    frappe.errprint(json_data)
-                    return
-    tds_section = frappe.db.get_value('Supplier', doc.supplier, 'tax_withholding_category.tds_section')
-    frappe.errprint(tds_section)
-    if tds_section:
-        non_pan_section = frappe.db.get_value('Tax Withholding Category', {'entity_type': 'No PAN / Invalid PAN', 'tds_section': tds_section})
-        if non_pan_section:
-            frappe.msgprint('Ensure correct PAN details. Applying non-PAN TDS section.')
-            doc.tax_withholding_category = non_pan_section
-        else:
-            frappe.msgprint(f'Non-PAN TDS section {tds_section} not found.')
+    if 'error' in json_data['message']:
+        error_message = json_data['message']['error']['message']
+        frappe.throw(f"API error occurred: {error_message}")
+    else:
+        if (json_data['message']['status'] == 'SUCCESS' and
+                json_data['message']['kycStatus'] == 'SUCCESS' and
+                json_data['message']['kycResult']['idStatus'] == 'VALID' and
+                json_data['message']['kycResult']['panStatus'] == 'VALID'):
+            return
+        # Applying non-PAN TDS to inactive PAN status
+        tds_section = frappe.db.get_value('Supplier', doc.supplier, 'tax_withholding_category.tds_section')
+        if tds_section:
+            non_pan_section = frappe.db.get_value('Tax Withholding Category', {'entity_type': 'No PAN / Invalid PAN', 'tds_section': tds_section})
+            if non_pan_section:
+                frappe.msgprint('Ensure correct PAN details. Applying non-PAN TDS section.')
+                doc.tax_withholding_category = non_pan_section
+            else:
+                frappe.msgprint(f'Non-PAN TDS section {tds_section} not found.')

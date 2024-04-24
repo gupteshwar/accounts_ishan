b4263f82cedcc60e665dc3f3026b63f9370f2899
rehan.a@indictranstech.com <rehan.a@indictranstech.com>
2024-04-24 12:12:54 +0530

    fix: api logic

--- accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
+++ accounts_ishan/accounts_ishan/custom_script/purchase_invoice.py
@@ -19,7 +19,8 @@ def before_save(doc,method=None):
     json_data = response.json()
     if 'error' in json_data['message']:
         error_message = json_data['message']['error']['message']
-        frappe.throw(f"API error occurred: {error_message}")
+        frappe.msgrpint(f"API error occurred: {error_message}")
+        return
     else:
         if (json_data['message']['status'] == 'SUCCESS' and
                 json_data['message']['kycStatus'] == 'SUCCESS' and

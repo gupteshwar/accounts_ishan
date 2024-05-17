d99a902f80ed6f03019f1af33bd66754c17da039
rehandev26 <165145630+rehandev26@users.noreply.github.com>
2024-05-17 17:13:05 +0530

    Update payment_entry.py

--- accounts_ishan/accounts_ishan/custom_script/payment_entry.py
+++ accounts_ishan/accounts_ishan/custom_script/payment_entry.py
@@ -1,6 +1,8 @@
 import frappe
 
 def before_save(doc, method=None):
+    if not doc.mode_of_payment:
+        return
     MODE_OF_PAYMENT = doc.mode_of_payment.upper()
     if MODE_OF_PAYMENT == "CHEQUE":
         doc.custom_payment_status = "In-Process"
